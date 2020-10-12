4a3d6d0b98a1d6a526b1b1fb5a91e1e8dc102a31
易成杰 <626659321@qq.com>
2020-10-12 20:51:18 +0800

    保存

---
+++ spring-cloud-gateway-service/src/main/java/com/yicj/gateway/GatewayServiceApp.java
@@ -0,0 +1,12 @@
+package com.yicj.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApp.class, args) ;
+    }
+}
