1a3565b8891fdf8bf96ea7157dad0f7a6b9d632b
易成杰 <626659321@qq.com>
2020-10-16 21:08:39 +0800

    保存

---
+++ spring-boot-nacos-discovery/src/main/java/com/yicj/nacos/SpringBootNacosDiscoveryApp.java
@@ -0,0 +1,12 @@
+package com.yicj.nacos;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNacosDiscoveryApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNacosDiscoveryApp.class, args) ;
+    }
+}
