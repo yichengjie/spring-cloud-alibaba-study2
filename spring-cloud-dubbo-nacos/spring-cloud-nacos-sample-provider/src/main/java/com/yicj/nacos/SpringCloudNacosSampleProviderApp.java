7049dfc60df77158e8a5c8a327518f73aa1bbd1a
易成杰 <626659321@qq.com>
2020-10-17 11:04:50 +0800

    备份

--- spring-cloud-dubbo-nacos/spring-cloud-nacos-sample-provider/src/main/java/com/yicj/nacos/SpringCloudNacosSampleProviderApp.java
+++ spring-cloud-dubbo-nacos/spring-cloud-nacos-sample-provider/src/main/java/com/yicj/nacos/SpringCloudNacosSampleProviderApp.java
@@ -2,7 +2,9 @@ package com.yicj.nacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudNacosSampleProviderApp {
 
--- spring-cloud-dubbo-nacos/spring-cloud-nacos-sample-provider/src/main/java/com/yicj/nacos/service/impl/HelloServiceImpl.java
+++ spring-cloud-dubbo-nacos/spring-cloud-nacos-sample-provider/src/main/java/com/yicj/nacos/service/impl/HelloServiceImpl.java
@@ -1,7 +1,9 @@
 package com.yicj.nacos.service.impl;
 
 import com.yicj.nacos.service.IHelloService;
+import org.apache.dubbo.config.annotation.Service;
 
+@Service
 public class HelloServiceImpl implements IHelloService {
     @Override
     public String sayHello(String name) {
