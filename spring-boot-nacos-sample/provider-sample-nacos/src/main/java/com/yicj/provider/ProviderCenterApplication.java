b601666da7bb5b9d7a8a4b563830c92e87bf5c7c
易成杰 <626659321@qq.com>
2020-10-18 14:47:40 +0800

    保存

--- spring-boot-nacos-sample/provider-sample-nacos/src/main/java/com/yicj/provider/ProviderCenterApplication.java
+++ spring-boot-nacos-sample/provider-sample-nacos/src/main/java/com/yicj/provider/ProviderCenterApplication.java
@@ -1,11 +1,8 @@
 package com.yicj.provider;
 
-import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//https://www.cnblogs.com/throwable/p/12134282.html
-//@DubboComponentScan
 @SpringBootApplication
 public class ProviderCenterApplication {
 
