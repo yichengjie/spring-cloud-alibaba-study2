f9d28aa3e451ee8799aab0a1f3ff61238591eae5
易成杰 <626659321@qq.com>
2020-10-20 16:28:18 +0800

    保存

--- spring-boot-seata-sample/sample-rest-web/src/main/java/com/yicj/seata/web/RestWebApplication.java
+++ spring-boot-seata-sample/sample-rest-web/src/main/java/com/yicj/seata/web/RestWebApplication.java
@@ -3,6 +3,7 @@ package com.yicj.seata.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//https://shirenchuang.blog.csdn.net/article/details/106739176
 //https://blog.csdn.net/github_38924695/article/details/108995886
 //https://blog.csdn.net/hosaos/article/details/89136666
 //https://blog.csdn.net/lifetragedy/article/details/104060200
