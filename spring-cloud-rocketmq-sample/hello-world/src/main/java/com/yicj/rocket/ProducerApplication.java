082654f942cbd4a2a5e231afcfdae258f775e9d3
易成杰 <626659321@qq.com>
2020-10-22 12:56:23 +0800

    spring cloud版本号统一升级

--- spring-cloud-rocketmq-sample/hello-world/src/main/java/com/yicj/rocket/ProducerApplication.java
+++ spring-cloud-rocketmq-sample/hello-world/src/main/java/com/yicj/rocket/ProducerApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 
+
+//https://blog.csdn.net/qq_27808011/article/details/80108622
 @EnableBinding({Source.class})
 @SpringBootApplication
 public class ProducerApplication {
