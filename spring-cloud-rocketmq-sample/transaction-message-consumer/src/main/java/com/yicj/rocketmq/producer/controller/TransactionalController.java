af214012e8e5051e0e6512d73b34dd32d72accda
易成杰 <626659321@qq.com>
2020-10-24 21:25:27 +0800

    保存

---
+++ spring-cloud-rocketmq-sample/transaction-message-consumer/src/main/java/com/yicj/rocketmq/producer/controller/TransactionalController.java
@@ -0,0 +1,35 @@
+package com.yicj.rocketmq.producer.controller;
+
+import com.yicj.rocketmq.producer.model.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.apache.rocketmq.spring.support.RocketMQHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.binder.BinderHeaders;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+public class TransactionalController {
+    @Autowired
+    private Source source;
+
+    @GetMapping("/transactional")
+    public String transactional() {
+        Order order = new Order("123", "浙江杭州") ;
+        String transactionId = UUID.randomUUID().toString() ;
+        MessageBuilder builder = MessageBuilder.withPayload(order)
+                .setHeader(RocketMQHeaders.TRANSACTION_ID, transactionId) ;
+        Message message = builder.build() ;
+        source.output().send(message) ;
+        return "OK";
+    }
+
+}
---
+++ spring-cloud-rocketmq-sample/transaction-message-consumer/src/main/java/com/yicj/rocketmq/producer/listener/TransactionMsgListener.java
@@ -0,0 +1,50 @@
+package com.yicj.rocketmq.producer.listener;
+
+import com.yicj.rocketmq.producer.model.Order;
+import org.apache.rocketmq.spring.annotation.RocketMQTransactionListener;
+import org.apache.rocketmq.spring.core.RocketMQLocalTransactionListener;
+import org.apache.rocketmq.spring.core.RocketMQLocalTransactionState;
+import org.apache.rocketmq.spring.support.RocketMQHeaders;
+import org.springframework.messaging.Message;
+
+
+// 实现RocketMQLocalTransactionListener接口，使用@RocketMQTransactionListener注解用于接收本地事务的监听
+// txProducerGroup是事务分组名称，和前面定义的OrderTransactionGroup保持一致,
+@RocketMQTransactionListener(txProducerGroup = "OrderTransaction")
+public class TransactionMsgListener implements RocketMQLocalTransactionListener {
+    @Override
+    public RocketMQLocalTransactionState executeLocalTransaction(Message msg, Object arg) {
+        try {
+            // 获取前面生成的事务id
+            String transactionId = (String) msg.getHeaders().get(RocketMQHeaders.TRANSACTION_ID) ;
+            // 以事务id为主键，执行本地事务
+            Order order = (Order)msg.getPayload();
+            boolean result = this.saveOrder(order, transactionId) ;
+            return result ? RocketMQLocalTransactionState.COMMIT : RocketMQLocalTransactionState.ROLLBACK;
+        }catch (Exception e){
+            return RocketMQLocalTransactionState.ROLLBACK ;
+        }
+    }
+
+    private boolean saveOrder(Order order, String transactionId) {
+        // 将事务ID设置为唯一键
+        // 调用数据库 insert to 订单表
+        return true ;
+    }
+
+    @Override
+    public RocketMQLocalTransactionState checkLocalTransaction(Message msg) {
+        // 获取事务id
+        String transactionId = (String) msg.getHeaders().get(RocketMQHeaders.TRANSACTION_ID) ;
+        // 以事务id为主键，查询本地事务主执行情况
+        if (isSuccess(transactionId)){
+            return RocketMQLocalTransactionState.COMMIT ;
+        }
+        return RocketMQLocalTransactionState.ROLLBACK;
+    }
+
+    private boolean isSuccess(String transactionId) {
+        // 查询数据库 select from 订单表
+        return true ;
+    }
+}
