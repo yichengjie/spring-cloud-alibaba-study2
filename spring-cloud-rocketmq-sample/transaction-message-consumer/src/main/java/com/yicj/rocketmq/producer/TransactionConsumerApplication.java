275413ba9a2e1adb15d26c810335236b1bc62d58
易成杰 <626659321@qq.com>
2020-10-25 20:54:16 +0800

    保存

--- spring-cloud-rocketmq-sample/transaction-message-consumer/src/main/java/com/yicj/rocketmq/producer/TransactionConsumerApplication.java
+++ spring-cloud-rocketmq-sample/transaction-message-consumer/src/main/java/com/yicj/rocketmq/producer/TransactionConsumerApplication.java
@@ -1,17 +1,11 @@
 package com.yicj.rocketmq.producer;
 
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.context.annotation.Bean;
-
-import java.io.IOException;
 
 
 @Slf4j
