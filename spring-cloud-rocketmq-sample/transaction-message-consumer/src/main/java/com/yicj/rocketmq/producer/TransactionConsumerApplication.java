c2aaee7cc25b020fc19ab4e14c64c989867edf97
易成杰 <626659321@qq.com>
2020-10-25 11:08:36 +0800

    保存

--- spring-cloud-rocketmq-sample/transaction-message-consumer/src/main/java/com/yicj/rocketmq/producer/TransactionConsumerApplication.java
+++ spring-cloud-rocketmq-sample/transaction-message-consumer/src/main/java/com/yicj/rocketmq/producer/TransactionConsumerApplication.java
@@ -1,11 +1,13 @@
 package com.yicj.rocketmq.producer;
 
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+
 
 @Slf4j
 @EnableBinding({Sink.class})
@@ -20,14 +22,4 @@ public class TransactionConsumerApplication {
 
         log.info("TopicTest receive : {}, receiveTime = {}", receiveMsg, System.currentTimeMillis());
     }
-
-   /* @Bean
-    public ObjectMapper objectMapper(){
-        ObjectMapper mapper = new ObjectMapper() ;
-        // 转换为格式化的json
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
-        // 如果json中有新增的字段并且是实体类类中不存在的，不报错
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        return mapper;
-    }*/
 }
